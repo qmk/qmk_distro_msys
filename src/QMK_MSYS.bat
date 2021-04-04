11f5fb951dee617a6f678b1c07701c2af6860243
zvecr <git@zvecr.com>
2021-04-04 20:44:06 +0100

    Add launcher script

--- src/QMK_MSYS.bat
+++ src/QMK_MSYS.bat
@@ -1,2 +1,2 @@
 @echo off
-%0\..\conemu\ConEmu64.exe -NoSingle -NoUpdate -icon %0\..\icon.ico -title "QMK MSYS" -run %0\..\usr\bin\bash.exe -l -i -cur_console:m:""
+START "" "%~dp0conemu\ConEmu64.exe" -NoSingle -NoUpdate -icon %~dp0icon.ico -title "QMK MSYS" -run %~dp0usr\bin\bash.exe -l -i -cur_console:m:""
